7f0f720cc714034720820e847393c298c5c8775a
miamiww <rivendalejones@gmail.com>
2020-05-28 23:11:40 -0400

    hopefully fixing quotes

--- db_updater.py
+++ db_updater.py
@@ -2,11 +2,11 @@ from cassandra.cluster import Cluster
 cluster = Cluster()
 session = cluster.connect('ipdatabase')
 
-company_names = ["Amazon","Google","Microsoft","Facebook","Apple"]
+company_names = ['Amazon','Google','Microsoft','Facebook','Apple']
 
 company_name = company_names[0]
 print company_name
-ip_address = "99.84.115.72"
+ip_address = '99.84.115.72'
 
 # to put data in the database
 result = session.execute("insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),{},{})".format(company_name,ip_address))
