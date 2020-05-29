e3a962e690df126e619e894ef1bff52e6769058e
miamiww <rivendalejones@gmail.com>
2020-05-29 00:12:27 -0400

    idk6

--- test_delete.py
+++ test_delete.py
@@ -3,7 +3,8 @@ cluster = Cluster()
 session = cluster.connect('ipdatabase')
 
 # input for a uuid
-id_value = input("Type UUID of record you wish to delete: ")
+id_value = raw_input("Type UUID of record you wish to delete: ")
+print id_value
 # deletes the value 
 result = session.execute("DELETE FROM ipdatabase.ips WHERE id=%s" % (id_value))
 print result
