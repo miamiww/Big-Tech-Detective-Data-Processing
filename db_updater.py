240897900896e4ba86338d297864dcc2c31d471e
miamiww <rivendalejones@gmail.com>
2020-05-28 23:13:34 -0400

    idk2

--- db_updater.py
+++ db_updater.py
@@ -9,5 +9,7 @@ print company_name
 ip_address = '99.84.115.72'
 
 # to put data in the database
-result = session.execute("insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),{},{})".format(company_name,ip_address))
+# result = session.execute("insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),{},{})".format(company_name,ip_address))
+result = session.execute("insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),'Amazon','99.84.115.72')")
+
 print result
