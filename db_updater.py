bd76cbfa29f82ddfa9db7eeb53a4e41fb2d991e3
miamiww <rivendalejones@gmail.com>
2020-05-29 00:02:20 -0400

    maybe

--- db_updater.py
+++ db_updater.py
@@ -11,7 +11,7 @@ ip_address = '13.226.27.72'
 # to put data in the database
 # result = session.execute("insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),{},{})".format(company_name,ip_address))
 # cql_string = "insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),%s,%s)" % (company_name,ip_address)
-cql_string = "insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),"+str(company_name)+","+str(ip_address)+")"
+cql_string = "insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),'"+str(company_name)+"','"+str(ip_address)+"')"
 print cql_string
 result = session.execute(cql_string)
 # result = session.execute("insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),'Amazon','13.226.27.72')")
