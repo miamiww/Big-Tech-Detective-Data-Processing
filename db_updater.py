701eb1a836f57286830fbee74dd9a244643309d1
miamiww <rivendalejones@gmail.com>
2020-05-28 23:24:43 -0400

    who knows

--- db_updater.py
+++ db_updater.py
@@ -5,11 +5,12 @@ session = cluster.connect('ipdatabase')
 company_names = ['Amazon','Google','Microsoft','Facebook','Apple']
 
 company_name = 'Amazon'
-print company_name
-ip_address = '99.84.115.72'
+# print company_name
+ip_address = '13.226.27.72'
 
 # to put data in the database
 # result = session.execute("insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),{},{})".format(company_name,ip_address))
-result = session.execute("insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),'Amazon','99.84.115.72')")
+result = session.execute("insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),%s,%s)" % (company_name,ip_address))
+# result = session.execute("insert into ipdatabase.ips(id,company,ipv4) VALUES(uuid(),'Amazon','99.84.115.72')")
 
 print result
